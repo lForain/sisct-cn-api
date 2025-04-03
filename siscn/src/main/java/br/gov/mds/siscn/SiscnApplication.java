423d556efbbac08baad868bfd7b1845c81eb64a7
¨Lucas¨ <lucasforain@gmail.com>
2025-04-03 16:41:11 -0300

    initializer

---
+++ siscn/src/main/java/br/gov/mds/siscn/SiscnApplication.java
@@ -0,0 +1,13 @@
+package br.gov.mds.siscn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiscnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiscnApplication.class, args);
+	}
+
+}
---
+++ siscn/src/test/java/br/gov/mds/siscn/SiscnApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.mds.siscn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiscnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
